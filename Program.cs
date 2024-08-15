277c92cb1662dbfadc35f02c5d511c606e26145c
unknown <islombekyoqubov33@gmail.com>
2024-08-15 23:28:50 +0500

    Ab

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+string firstString = Console.ReadLine();
+string symbol = Console.ReadLine();
+string secondString = Console.ReadLine();
+int firsNumber = Convert.ToInt32(firstString);
+int secondNumber = Convert.ToInt32(secondString);
+if(symbol == "+") System.Console.WriteLine(firsNumber + secondNumber);
+else if(symbol == "-") Console.WriteLine(firsNumber - secondNumber);
+else if(symbol == "*") System.Console.WriteLine(firsNumber * secondNumber);
+else if(symbol == "/") System.Console.WriteLine(firsNumber / secondNumber);
